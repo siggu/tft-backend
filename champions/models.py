47637911d702cb947fb90023063b754e9bf5b668
siggu <siggu100@gmail.com>
2024-03-04 21:09:19 +0900

    champions

--- champions/models.py
+++ champions/models.py
@@ -10,6 +10,30 @@ class Champion(models.Model):
     synergies = models.ManyToManyField(
         "synergies.Synergy",
     )
+    health = models.PositiveIntegerField(
+        default=100,
+    )
+    offense_power = models.PositiveIntegerField(
+        default=10,
+    )
+    dps = models.PositiveIntegerField(
+        default=10,
+    )
+    attack_range = models.PositiveIntegerField(
+        default=1,
+    )
+    attack_speed = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        default=1,
+    )
+    defense = models.PositiveIntegerField(
+        default=10,
+    )
+    magic_resistance = models.PositiveIntegerField(
+        default=10,
+    )
+    avatar = models.ImageField(null=True)
 
     def __str__(self):
         return self.name
