8cd7f03cf732ada527507b00c563bdaac6ddbece
siggu <siggu100@gmail.com>
2024-03-17 00:43:44 +0900

    synergy

--- synergies/views.py
+++ synergies/views.py
@@ -5,7 +5,7 @@ from rest_framework.exceptions import NotFound
 
 
 from . import serializers
-from .models import Origin, Job
+from .models import Origin, Job, SynergyTier, SynergyStack
 
 
 # Create your views here.
@@ -18,21 +18,24 @@ class Origins(APIView):
         )
         return Response(serializer.data)
 
-    
+
 class OriginName(APIView):
-    def get_object(self,name):
+    def get_object(self, name):
         try:
             return Origin.objects.get(name=name)
         except Origin.DoesNotExist:
             raise NotFound
-        
-    def get(self, reqeust,name):
+
+    def get(self, reqeust, name):
         origin = self.get_object(name)
         origin_data = []
         origin_info = {
-            'id': origin.id,
-            'name': origin.name,
-            'synergy_champions': [{"champion_name":champion.name, "champion_pk":champion.pk} for champion in origin.champions.all()]  # 여기서 champions Champion 모델에서 Origin에 대한 역참조 이름입니다.
+            "id": origin.id,
+            "name": origin.name,
+            "synergy_champions": [
+                {"champion_name": champion.name, "champion_pk": champion.pk}
+                for champion in origin.champions.all()
+            ],  # 여기서 champions Champion 모델에서 Origin에 대한 역참조 이름입니다.
         }
         origin_data.append(origin_info)
 
@@ -49,15 +52,36 @@ class Jobs(APIView):
         )
         return Response(serializer.data)
 
+
 class JobName(APIView):
-    def get_object(self,name):
+    def get_object(self, name):
         try:
             return Job.objects.get(name=name)
         except Job.DoesNotExist:
             raise NotFound
-        
-    def get(self, reqeust,name):
+
+    def get(self, reqeust, name):
         job = self.get_object(name)
 
         serializer = serializers.JobSerializer(job)
+        return Response(serializer.data)
+
+
+class SynergyTier(APIView):
+    def get(self, request):
+        synergyTiers = SynergyTier.objects.all()
+        serializer = serializers.SynergyTierSerializer(
+            synergyTiers,
+            many=True,
+        )
+        return Response(serializer.data)
+
+
+class SynergyStack(APIView):
+    def get(self, request):
+        synergyStacks = SynergyStack.objects.all()
+        serializer = serializers.SynergyStackSerializer(
+            synergyStacks,
+            many=True,
+        )
         return Response(serializer.data)
