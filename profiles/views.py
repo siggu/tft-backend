c175fdbe6ab43c90fe2fda207d20488b9ee18f0f
siggu <106001755+siggu@users.noreply.github.com>
2024-04-29 17:47:02 +0900

    profile views 코드 변경
    소환사 puuid로 검색하도록 변경

--- profiles/views.py
+++ profiles/views.py
@@ -99,8 +99,8 @@ class SummonerMathcesByPuuidAPIView(APIView):
                 {"error": "SummonerPuuid not found"}, status=status.HTTP_404_NOT_FOUND
             )
 
-        # api_key = os.getenv("RIOT_API_KEY")
-        url = f"https://asia.api.riotgames.com/tft/match/v1/matches/by-puuid/{summonerName}/ids?start=0&count=20&api_key={api_key}"
+        api_key = os.getenv("RIOT_API_KEY")
+        url = f"https://asia.api.riotgames.com/tft/match/v1/matches/by-puuid/{summoner_instance.puuid}/ids?start=0&count=3&api_key={api_key}"
 
         try:
             response = requests.get(url)
