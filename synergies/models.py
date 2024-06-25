194e97a4f6a5ecc437ca7801ac21e4f147ebcc2a
blueconecell <95961799+blueconecell@users.noreply.github.com>
2024-06-25 18:46:57 +0900

    5주년 시너지 추가 완료

--- synergies/models.py
+++ synergies/models.py
@@ -8,8 +8,8 @@ class Trait(models.Model):
         max_length=20,
         primary_key=True,
     )
-    ingameKey = models.CharField(max_length=20)
-    name = models.CharField(max_length=10)
+    ingameKey = models.CharField(max_length=40)
+    name = models.CharField(max_length=20)
     desc = models.TextField(
         max_length=300,
         blank=True,
